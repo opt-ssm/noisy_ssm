97d0c84bc1cce5170ea75b5d13592aaa4581e7e1
Andron00e <semenov.andrei.v@gmail.com>
2024-07-22 17:30:38 +0300

    ref models has been added

--- configs.py
+++ configs.py
@@ -1,7 +1,52 @@
+import random
+
+import numpy as np
 import peft
 import torch
 import transformers
 
 
 class Constants:
-    pass
+    # general
+    lr = 3e-4
+    batch_size = 64
+    # Mamba 1 models
+    mamba_1_130m = "state-spaces/mamba-130m-hf"
+    mamba_1_370m = "state-spaces/mamba-370m-hf"
+    mamba_1_790m = "state-spaces/mamba-790m-hf"
+    mamba_1_4b = "state-spaces/mamba-1.4b-hf"
+    # Mamba 2 models
+    mamba_2_4b = "state-spaces/mamba-1.4b-hf"
+    mamba_2_7b = "state-spaces/mamba2-2.7b"
+    mamba_2_7b_attn = "state-spaces/mamba2attn-2.7b"
+    mamba_2_8b = "state-spaces/mamba-2.8b-hf"
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def set_one_device(device_no: int):
+    if torch.cuda.is_available():
+        device = torch.device(f"cuda:{device_no}")
+        print("There are %d GPU(s) available." % torch.cuda.device_count())
+        print("We will use the GPU:", torch.cuda.get_device_name(device_no))
+    else:
+        print("No GPU available, using the CPU instead.")
+        device = torch.device("cpu")
+    return device
+
+
+def print_trainable_parameters(model):
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params:,} || all params: {all_param:,} || trainable%: {100 * trainable_params / all_param:.2f}"
+    )
